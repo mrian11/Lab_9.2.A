2ca1fd386f8d07bd094a61b55595833a1e1be482
mrian11 <marian.yatsyk.ri.2023@lpnu.ua>
2023-11-30 19:19:46 +0200

    Lab_9.2.a

---
+++ UnitTest_Lab_9.2.A/UnitTest_Lab_9.2.A.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include"../Lab_9.2.A/LAB.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab92A
+{
+	TEST_CLASS(UnitTestLab92A)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int N = 3;
+			Student student[N];
+
+			student[0] = { "Яцик", 1, KN,  5, 4, 4 };
+			student[1] = { "Артюх", 1, PF, 5, 5, 4 };
+			student[2] = { "Цекот", 1, PF, 4, 5, 5 };
+			//student[2] = { "Артюх", 1, PF, 5, 5, 4 };
+
+			Assert::AreEqual(BinSearch(student, N, "Артюх", PF, 5), 1);
+
+		}
+	};
+}
